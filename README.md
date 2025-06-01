2335243b12b2024db6bfe8048120d1c5f3a6792e
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-06-01 14:01:05 -0600

    v8.0.0, Created Solution

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # HTMLCommon
+
+## Release Notes
+**8.0.0**
+- Added `HtmlColorEnum` to represent HTML Named Colors.
+  - Supports all 140 HTML colors based on [w3schools](https://www.w3schools.com/colors/colors_names.asp) and [w3.org](https://www.w3.org/TR/css-color-4/#named-colors).
+  - Includes a `ToHex` extension method that returns the Html Hex string.
+    - For example, `HtmlColorName.AliceBlue.ToHex()` returns `"#F0F8FF"`.
---
+++ src/Marqdouj.HtmlCommon/Tests/HtmlColorNameTests.cs
@@ -0,0 +1,25 @@
+﻿using Marqdouj.HtmlCommon;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class HtmlColorNameTests
+    {
+        [TestMethod]
+        public void HtmlColorName_ToHex_AllNamesCovered()
+        {
+            // Arrange
+            var names = Enum.GetValues(typeof(HtmlColorName)).Cast<HtmlColorName>().ToList();
+
+            foreach (var name in names)
+            {
+                // Act
+                var hex = name.ToHex(); //Will throw exception if the name is not supported in ToHex
+
+                // Assert
+                Assert.IsTrue(hex.StartsWith('#'), $"Hex for {name} should start with # but was {hex}"); // Fixed CA1866
+                Assert.AreEqual(7, hex.Length, $"Hex for {name} should be 7 characters long but was {hex.Length} characters");
+            }
+        }
+    }
+}
